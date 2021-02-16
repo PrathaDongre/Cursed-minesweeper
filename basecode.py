fe0f83ab73a6107bf7af7b1c6d1efad065c9aed4
PrathaDongre <56888850+PrathaDongre@users.noreply.github.com>
2021-02-16 13:26:05 +0530

    Sanatan added comments

--- basecode.py
+++ basecode.py
@@ -4,10 +4,13 @@ from itertools import permutations
 import math as m
 import matplotlib.pyplot as plt
 
+#this just defines a nxn matrix with zeros everywhere
 def minefield(n):
     minefield_ = np.zeros(n**2,dtype = int).reshape(n,n)
     return minefield_
 
+#this is a list of the n! different distributions of mines, given the condition that each row and column has exactly one mine
+#so each element of this list is a collection of n coordinates where we are going to put the mines
 def mine_perm(n):
     list_ = []
 
@@ -28,6 +31,9 @@ def mine_perm(n):
         list_.append(list_temp)
     return list_
 
+#this defines your final nxn gameboard with zeros everywhere excpet the mines, which are represented by 100
+#the value k specifies which permutation from mine_perm you want to use on the board
+#so using k, you basically choose your mine distribution (you have n! different distributions to choose from)
 def gameboard(n,k):
     mine_list = mine_perm(n)[k]
     board = minefield(n)
@@ -37,6 +43,14 @@ def gameboard(n,k):
         board[a,b]=100
     return(board)
 
+#this is the moving strategy
+#i have designed keeping in mind that the objective is to reach the bottom right of the board after initializing at the top left
+#say you standing in the middle of the board, you will have 8 adjacent squares that you can see
+#the player cant move to a square that has a mine, and ofc it cant move out of the board
+#it follows the priority order --> (down + right, down, right, down + left, up + right, left, up, left + up)
+#so whichever of the 8 squares are available, it will move to the one which has the highest priority order among them
+#whenever the player enters a square you add 1 to its value, and do the same when it leaves a square. so the position of the player is whichever square has an odd value
+#using this function on a board only makes one move. if you want five moves you have to use the function five times
 def moving_strat_1(board):
     board_dash = board.copy()
     n = board_dash.shape[0]
@@ -64,11 +78,15 @@ def moving_strat_1(board):
     board_dash[next] = board_dash[next] + 1
     return board_dash
 
+#this puts your player on the board on the specified coordinates given by i,j
 def initialize(board,i,j):
     board_dash = board.copy()
     board_dash[i,j] = board_dash[(i,j)] + 1
     return board_dash
 
+#using this function on a given board will run the moving_strat_1 till the player reaches the bottom right square.
+#it gives the final board and the number of steps it has taken to reach the destination
+#the different numbers on the board allow you to see the number of times a square has been visited and hence allow you to trace the path it has followed
 def game_strat_1(board):
     board_dash = board.copy()
     n = board_dash.shape[0]
@@ -81,6 +99,9 @@ def game_strat_1(board):
             counter = counter + 1
         return board_dash,counter
 
+#for a given n, this plots the number of steps taken to reach the destination using moving_strat_1 for different mine distributions on a nxn board
+#so we have n! different possible mine distributions but I have removed those distributions in which either the top left or the bottom right square was a mine
+#so the x-axis will have a little less than n! values
 def plot_game_strat_1(n):
     allowed_mines = list(np.arange(0,m.factorial(n)))
     allowed_mines_copy = allowed_mines.copy()
